4f2c05edcde00f14050075819b44c7d9ff736fb6
Ramanpreet <ramnapreet230@gmail.com>
2019-09-28 16:54:06 -0400

    [Ramanpreet] Added Employee class.

---
+++ src/main/java/com/lambton/EmployeeClass.java
@@ -0,0 +1,5 @@
+package com.lambton;
+
+//Created by Ramanpreet Singh
+public class EmployeeClass {
+}
--- src/main/java/com/lambton/MainClass.java
+++ src/main/java/com/lambton/MainClass.java
@@ -6,6 +6,5 @@ public class MainClass {
     // Main function
     public static void main(String[] args)
     {
-
     }
 }
