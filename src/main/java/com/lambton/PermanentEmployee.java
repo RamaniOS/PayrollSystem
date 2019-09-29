db2a15ad6e191cd4e6b1e708e4f2115bedb3e69e
Ramanpreet <ramnapreet230@gmail.com>
2019-09-28 22:23:04 -0400

    [Ramanpreet] Added properties in Permanent Employee Class.

--- src/main/java/com/lambton/PermanentEmployee.java
+++ src/main/java/com/lambton/PermanentEmployee.java
@@ -2,4 +2,39 @@ package com.lambton;
 
 //Created by Ramanpreet Singh
 public class PermanentEmployee extends EmployeeClass {
+
+    float salary;
+    float bonus;
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public float getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(float bonus) {
+        this.bonus = bonus;
+    }
+
+    public PermanentEmployee(float salary, float bonus) {
+        this.salary = salary;
+        this.bonus = bonus;
+    }
+
+    public PermanentEmployee(String name, int age, float salary, float bonus) {
+        super(name, age);
+        this.salary = salary;
+        this.bonus = bonus;
+    }
+
+    public float calculateEarnings()
+    {
+        return getSalary() + getBonus();
+    }
 }
