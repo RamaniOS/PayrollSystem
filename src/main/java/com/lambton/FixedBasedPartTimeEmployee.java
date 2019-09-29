99e7c5b9bb880e93dfdfc68f326f7985cac9d5c6
Ramanpreet <ramnapreet230@gmail.com>
2019-09-28 22:26:45 -0400

    [Ramanpreet] Added properties in FixedBasedPartTime Employee Class.

--- src/main/java/com/lambton/FixedBasedPartTimeEmployee.java
+++ src/main/java/com/lambton/FixedBasedPartTimeEmployee.java
@@ -2,4 +2,27 @@ package com.lambton;
 
 //Created by Ramanpreet Singh
 public class FixedBasedPartTimeEmployee extends PartTimeEmployee {
+
+    float fixedAmount;
+
+    public float getFixedAmount() {
+        return fixedAmount;
+    }
+
+    public void setFixedAmount(float fixedAmount) {
+        this.fixedAmount = fixedAmount;
+    }
+
+    public FixedBasedPartTimeEmployee(float rate, int hoursWorked) {
+        super(rate, hoursWorked);
+    }
+
+    public FixedBasedPartTimeEmployee(String name, int age, float rate, int hoursWorked) {
+        super(name, age, rate, hoursWorked);
+    }
+
+    public float calculateEarnings()
+    {
+        return (getRate() * getHoursWorked() + getFixedAmount());
+    }
 }
