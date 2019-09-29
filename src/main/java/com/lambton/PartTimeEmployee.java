e74cf282eef1f347699e81b172ca62919e79ed41
Ramanpreet <ramnapreet230@gmail.com>
2019-09-28 22:24:15 -0400

    [Ramanpreet] Added properties in PartTime Employee Class.

--- src/main/java/com/lambton/PartTimeEmployee.java
+++ src/main/java/com/lambton/PartTimeEmployee.java
@@ -2,4 +2,34 @@ package com.lambton;
 
 //Created by Ramanpreet Singh
 public class PartTimeEmployee extends EmployeeClass {
+
+    private float rate;
+    private int hoursWorked;
+
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(float rate) {
+        this.rate = rate;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public PartTimeEmployee(float rate, int hoursWorked) {
+        this.rate = rate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public PartTimeEmployee(String name, int age, float rate, int hoursWorked) {
+        super(name, age);
+        this.rate = rate;
+        this.hoursWorked = hoursWorked;
+    }
 }
