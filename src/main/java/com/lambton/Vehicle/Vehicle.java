575f773e63757849fa8042296e5e13a2898da376
Nitin Jaswal <nitinj@whizkraft.net>
2019-09-30 19:46:56 -0400

    [Nitin]Added Vehicle Type Package.

--- src/main/java/com/lambton/Vehicle/Vehicle.java
+++ src/main/java/com/lambton/Vehicle/Vehicle.java
@@ -1,5 +1,10 @@
+/*
+Created by: Nitin Jaswal
+ */
+
 package com.lambton.Vehicle;
 
+/// This is the Vehicle class, containing the information of Employee's Vehicle
 public class Vehicle {
 
 }
---
+++ src/main/java/com/lambton/VehicleType/Car.java
@@ -0,0 +1,4 @@
+package com.lambton.VehicleType;
+
+public class Car {
+}
