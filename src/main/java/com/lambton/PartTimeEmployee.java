6f1cd668365bc6433278d988f64a8b288f9ddfd2
Ramanpreet <ramnapreet230@gmail.com>
2019-09-30 17:46:20 -0400

    [Ramanpreet] Make fixes in PartTime Class.

--- src/main/java/com/lambton/PartTimeEmployee.java
+++ src/main/java/com/lambton/PartTimeEmployee.java
@@ -36,4 +36,10 @@ public abstract class PartTimeEmployee extends EmployeeClass {
         this.rate = rate;
         this.hoursWorked = hoursWorked;
     }
+
+    @Override
+    public void printMyData() {
+        System.out.println("Name:" + " " + getName());
+        System.out.println("Year of Birth:" + " " + getBirthYear());
+    }
 }
