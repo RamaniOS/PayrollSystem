7b07e18e730a3bfaca38ea22379f28e66a48e88d
Nitin Jaswal <nitinj@whizkraft.net>
2019-10-04 19:13:12 -0400

    [Nitin] fetching motorcycle detail1

--- src/main/java/com/lambton/NitinMain.java
+++ src/main/java/com/lambton/NitinMain.java
@@ -17,12 +17,16 @@ public class NitinMain {
         mC.printMyData();
 
         LocalDate insuranceDate2 = LocalDate.parse("2018-12-02");
-        MotorCycle mC2 = new MotorCycle("motorcycle", "Honda", "IOP23W", "Sports", insuranceDate1, 23.0f, 26000.00f, 1, "petrol");
+        MotorCycle mC2 = new MotorCycle("motorcycle", "Honda", "IOP23W", "Sports", insuranceDate2, 23.0f, 26000.00f, 1, "petrol");
         mC2.printMyData();
 
         LocalDate insuranceDate3 = LocalDate.parse("2018-12-03");
-        MotorCycle mC3 = new MotorCycle("motorcycle", "Honda", "IOP23W", "Sports", insuranceDate1, 23.0f, 26000.00f, 1, "petrol");
+        MotorCycle mC3 = new MotorCycle("motorcycle", "Honda", "IOP23W", "Sports", insuranceDate3, 23.0f, 26000.00f, 1, "petrol");
         mC3.printMyData();
+
+        LocalDate insuranceDate4 = LocalDate.parse("2018-12-04");
+        MotorCycle mC4 = new MotorCycle("motorcycle", "Honda", "IOP23W", "Sports", insuranceDate4, 23.0f, 26000.00f, 1, "petrol");
+        mC4.printMyData();
 
 
 
