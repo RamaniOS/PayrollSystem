cf5f8eec964b3e7291c5910440f6662102ca0933
Nitin Jaswal <nitinj@whizkraft.net>
2019-10-04 18:07:57 -0400

    [Nitin] printing data for car user 2

--- src/main/java/com/lambton/NitinMain.java
+++ src/main/java/com/lambton/NitinMain.java
@@ -10,5 +10,11 @@ public class NitinMain {
         LocalDate insuranceDate = LocalDate.parse("2018-12-03");
         Car c = new Car("car", "Tyota", "PDO123", "Classic", insuranceDate , 13.50f, 33000.00f, 2, "petrol");
         c.printMyData();
+
+        LocalDate insuranceDate1 = LocalDate.parse("2018-11-01");
+        Car c1 = new Car("car", "Mercedies", "PDO123", "Sports", insuranceDate , 13.50f, 33000.00f, 2, "petrol");
+        c1.printMyData();
+
+     //   .
     }
 }
